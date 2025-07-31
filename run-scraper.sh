36dc118d54f4cc12828b11194ae36c027de5aa9d
HarryLee02 <22520435@gm.uit.edu.vn>
2025-07-31 10:13:35 +0700

    Edit bash script for cronjob to work

--- run-scraper.sh
+++ run-scraper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# change this to your project absolute path for cronjob to work
+cd /home/harrylee/API-starter-pack/
+
 source .env
+
 docker run -d --rm \
   -v scraper-logs:/app/logs \
   -v docs-volume:/app/docs \
